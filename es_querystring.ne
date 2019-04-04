49f4570b4d695e13df7423a3ae2fdae095e28676
Simon Hildebrandt <simonhildebrandt@gmail.com>
2019-04-05 09:07:57 +1100

    Fixed whitespace

--- es_querystring.ne
+++ es_querystring.ne
@@ -3,37 +3,37 @@
 @builtin "whitespace.ne"
 
 MAIN -> _ clause _ {%
-	function (data, location, reject) {
-		return data[1][0];
-	}
+  function (data, location, reject) {
+    return data[1][0];
+  }
 %}
 
 clause -> grouped
-		    | bracketed
+        | bracketed
         | simple
 
 grouped -> clause __ logical __ clause {%
-	function (data, location, reject) {
-		return { type: "logical", start: location, operator: data[2], children: [data[0][0], data[4][0]] };
-	}
+  function (data, location, reject) {
+    return { type: "logical", start: location, operator: data[2], children: [data[0][0], data[4][0]] };
+  }
 %}
 
 simple -> match {%
-	function (data, location, reject) {
-		return { type: "simple", start: location, value: data[0][0] };
-	}
+  function (data, location, reject) {
+    return { type: "simple", start: location, value: data[0][0] };
+  }
 %}
 
 bracketed -> "(" _ clause _ ")" {%
-	function (data, location, reject) {
-		return { type: "bracketed", start: location, value: data[2][0] };
-	}
+  function (data, location, reject) {
+    return { type: "bracketed", start: location, value: data[2][0] };
+  }
 %}
 
 logical -> logicaloperator {%
-	function (data, location, reject) {
-		return { value: data[0][0], start: location };
-	}
+  function (data, location, reject) {
+    return { value: data[0][0], start: location };
+  }
 %}
 
 logicaloperator -> "AND"
@@ -46,47 +46,47 @@ match -> field_and_string
        | string
 
 field_and_string -> field ":" string {%
-	function (data, location, reject) {
-		return { type: 'field', start: location, field: data[0], value: data[2].value };
-	}
+  function (data, location, reject) {
+    return { type: 'field', start: location, field: data[0], value: data[2].value };
+  }
 %}
 
 field -> wordchar:+ {%
-	function (data, location, reject) {
-		return data[0].join("");
-	}
+  function (data, location, reject) {
+    return data[0].join("");
+  }
 %}
 
 string -> string_or_quoted_string {%
-	function (data, location, reject) {
-		return { type: 'field', start: location, field: null, value: data[0][0][0] };
-	}
+  function (data, location, reject) {
+    return { type: 'field', start: location, field: null, value: data[0][0][0] };
+  }
 %}
 
 string_or_quoted_string -> weightable_string weight:?
 
 weightable_string -> values
-                  | quoted_string
+                   | quoted_string
 
 weight -> "~" weight_number
 
 weight_number -> [0-9]
 
 values -> value:+ {%
-	function (data, location, reject) {
-		return { type: "literal", start: location, value: data[0].join("") };
-	}
+  function (data, location, reject) {
+    return { type: "literal", start: location, value: data[0].join("") };
+  }
 %}
 
 quoted_string -> "\"" value_or_space:+ "\"" {%
-	function (data, location, reject) {
-		return { type: "quoted", start: location, value: '"' + data[1].join("") + '"' };
-	}
+  function (data, location, reject) {
+    return { type: "quoted", start: location, value: '"' + data[1].join("") + '"' };
+  }
 %}
 
 value -> wordchar
        | "\\" escaped_value
-			 | wildcard
+       | wildcard
 
 value_or_space -> value
                 | " "
--- grammar.js
+++ grammar.js
@@ -14,7 +14,7 @@ var grammar = {
     {"name": "wschar", "symbols": [/[ \t\n\v\f]/], "postprocess": id},
     {"name": "MAIN", "symbols": ["_", "clause", "_"], "postprocess": 
         function (data, location, reject) {
-        	return data[1][0];
+          return data[1][0];
         }
         },
     {"name": "clause", "symbols": ["grouped"]},
@@ -22,22 +22,22 @@ var grammar = {
     {"name": "clause", "symbols": ["simple"]},
     {"name": "grouped", "symbols": ["clause", "__", "logical", "__", "clause"], "postprocess": 
         function (data, location, reject) {
-        	return { type: "logical", start: location, operator: data[2], children: [data[0][0], data[4][0]] };
+          return { type: "logical", start: location, operator: data[2], children: [data[0][0], data[4][0]] };
         }
         },
     {"name": "simple", "symbols": ["match"], "postprocess": 
         function (data, location, reject) {
-        	return { type: "simple", start: location, value: data[0][0] };
+          return { type: "simple", start: location, value: data[0][0] };
         }
         },
     {"name": "bracketed", "symbols": [{"literal":"("}, "_", "clause", "_", {"literal":")"}], "postprocess": 
         function (data, location, reject) {
-        	return { type: "bracketed", start: location, value: data[2][0] };
+          return { type: "bracketed", start: location, value: data[2][0] };
         }
         },
     {"name": "logical", "symbols": ["logicaloperator"], "postprocess": 
         function (data, location, reject) {
-        	return { value: data[0][0], start: location };
+          return { value: data[0][0], start: location };
         }
         },
     {"name": "logicaloperator$string$1", "symbols": [{"literal":"A"}, {"literal":"N"}, {"literal":"D"}], "postprocess": function joiner(d) {return d.join('');}},
@@ -54,19 +54,19 @@ var grammar = {
     {"name": "match", "symbols": ["string"]},
     {"name": "field_and_string", "symbols": ["field", {"literal":":"}, "string"], "postprocess": 
         function (data, location, reject) {
-        	return { type: 'field', start: location, field: data[0], value: data[2].value };
+          return { type: 'field', start: location, field: data[0], value: data[2].value };
         }
         },
     {"name": "field$ebnf$1", "symbols": ["wordchar"]},
     {"name": "field$ebnf$1", "symbols": ["field$ebnf$1", "wordchar"], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
     {"name": "field", "symbols": ["field$ebnf$1"], "postprocess": 
         function (data, location, reject) {
-        	return data[0].join("");
+          return data[0].join("");
         }
         },
     {"name": "string", "symbols": ["string_or_quoted_string"], "postprocess": 
         function (data, location, reject) {
-        	return { type: 'field', start: location, field: null, value: data[0][0][0] };
+          return { type: 'field', start: location, field: null, value: data[0][0][0] };
         }
         },
     {"name": "string_or_quoted_string$ebnf$1", "symbols": ["weight"], "postprocess": id},
@@ -80,14 +80,14 @@ var grammar = {
     {"name": "values$ebnf$1", "symbols": ["values$ebnf$1", "value"], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
     {"name": "values", "symbols": ["values$ebnf$1"], "postprocess": 
         function (data, location, reject) {
-        	return { type: "literal", start: location, value: data[0].join("") };
+          return { type: "literal", start: location, value: data[0].join("") };
         }
         },
     {"name": "quoted_string$ebnf$1", "symbols": ["value_or_space"]},
     {"name": "quoted_string$ebnf$1", "symbols": ["quoted_string$ebnf$1", "value_or_space"], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
     {"name": "quoted_string", "symbols": [{"literal":"\""}, "quoted_string$ebnf$1", {"literal":"\""}], "postprocess": 
         function (data, location, reject) {
-        	return { type: "quoted", start: location, value: '"' + data[1].join("") + '"' };
+          return { type: "quoted", start: location, value: '"' + data[1].join("") + '"' };
         }
         },
     {"name": "value", "symbols": ["wordchar"]},
